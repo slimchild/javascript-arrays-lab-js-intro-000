2b6d9bf1d8411889e48803f5d9b38b2d74047660
Timothy Wyld <timwyld@gmail.com>
2019-10-16 17:48:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ function prependKitten(kitten){
 
 function removeLastKitten(kitten){
   var kittens = ['Milo', 'Otis', 'Garfield'];
-  var moreKittens = kittens.slice(2,-1);
+  var moreKittens = kittens.slice(-1);
 return moreKittens;
 }
